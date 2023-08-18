aa43c714e643ee4d53fe73e31b8f25c05ed2d7f2
hashcookie <hashcookie404@gmail.com>
2023-08-18 22:57:08 +0800

    update

--- MIServerless.py
+++ MIServerless.py
@@ -5,7 +5,6 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
     }
  
-#Get logincode
 def get_code(location):
     code_pattern = re.compile("(?<=access=).*?(?=&)")
     code = code_pattern.findall(location)[0]
